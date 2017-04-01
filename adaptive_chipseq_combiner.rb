43dafec4b4443160f8c03f1b1fd2244a59e2e7aa
Ilya Vorontsov <prijutme4ty@gmail.com>
2017-04-01 14:23:50 +0300

    fix adaptive combiner to include one more quality case

--- adaptive_chipseq_combiner.rb
+++ adaptive_chipseq_combiner.rb
@@ -10,7 +10,7 @@ end
 FileUtils.mkdir_p 'gtrd/adaptive_quality'
 # take `quality_default_level` best datasets. If resulting file is empty, add dataset with the next quality level
 quality_default_level = 2
-quality_levels = ['highest', 'high', 'medium', 'low']
+quality_levels = ['highest', 'high', 'medium', 'low', 'lowest']
 tf_list = Dir.glob('gtrd/highest/*.bed').map{|fn| File.basename(fn, '.bed')[0..-3] }
 tf_list.each do |tf|
   tf_chipseq_filelist = (quality_default_level .. quality_levels.size).map{|quality_level|
